2c0eb65442c41a8747884fb4a4ece76360cb31bf
Alexander Serzhenko <alexsergspectr@gmail.com>
2020-11-11 20:14:35 +0200

    module 5 finish

---
+++ js/5-4.js
@@ -0,0 +1,35 @@
+// Write code under this line
+  class StringBuilder{
+      constructor(_value){
+          this._value=_value;
+      }
+      append(str){
+          this._value+=str;
+      };
+      prepend(str){
+          this._value=str+this._value;
+      };
+      pad(str){
+          this.prepend(str);
+          this.append(str);
+      };
+      get value(){
+        return this._value;
+      }
+    }
+  
+ 
+// console.log(typeof StringBuilder);
+// 'function'
+
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
